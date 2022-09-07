371dba84c5577e6a708811c4a2eee3670391ce46
Ayla <1348452410@qq.com>
2022-09-07 18:01:59 +0800

    提交

--- src/components/CategorySelect/index.vue
+++ src/components/CategorySelect/index.vue
@@ -2,19 +2,19 @@
   <div>
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
       <el-form-item label="一级分类">
-        <el-select v-model="formInline.category1Id" placeholder="请选择">
+        <el-select v-model="formInline.category1Id" placeholder="请选择" @change="getCategory2Id">
           <!-- v-for="(item,index) in " -->
-          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option  v-for="(item,index) in category1List" :value="item.id" :label="item.name" :key="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="formInline.category2Id" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
+        <el-select v-model="formInline.category2Id" placeholder="请选择" @change="getCategory3Id">
+          <el-option :label="item.name"  v-for="(item,index) in category2List" :key="item.id" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类">
         <el-select v-model="formInline.category3Id" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option :label="item.name"  v-for="(item,index) in category3List" :key="item.id" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <!-- <el-form-item>
@@ -39,6 +39,9 @@ export default {
         category2Id:"",
         category3Id:"",
       },
+      category1List:[],
+      category2List:[],
+      category3List:[],
     }
   },
   mounted() {
@@ -47,9 +50,26 @@ export default {
   methods: {
     getCategory1Id() {
       reqCategory1List().then((res) => {
-        console.log(res);
+        // console.log(res);
+        if(res.code === 200){
+          this.category1List = res.data;
+        }
       });
     },
+    getCategory2Id(){
+      reqCategory2List(this.formInline.category1Id).then((res)=>{
+       if(res.code === 200){
+          this.category2List = res.data;
+        }
+      })
+    },
+    getCategory3Id(){
+      reqCategory3List(this.formInline.category2Id).then((res)=>{
+       if(res.code === 200){
+          this.category3List = res.data;
+        }
+      })
+    }
   },
 };
 </script>
